e9cdab73026a499ae9ac3d41bbd8d2e336f7990a
oninotna <antoninomusarratubbi@gmail.com>
2025-06-17 13:03:50 +0200

    added reviews query in show controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,12 +2,22 @@ const connection = require('../data/db');
 
 const index = (req, res) => {
     const querySql = `
-    SELECT * 
-      FROM movies;`
+    SELECT 
+      movies.title,
+      movies.director,
+      movies.genre,
+      movies.release_year,
+      movies.abstract, 
+      movies.image
+        FROM movies;`
 
-    connection.query(querySql, (err, results) => {
+    connection.query(querySql, (err, moviesResults) => {
         if (err) return res.status(500).json({err: "Database query failed"});
-        res.json(results);
+        
+        const movies = moviesResults.map(movie => (
+            {...movie, image: `http://localhost:3000/img/movies/${movie.image}`}
+        ))
+        res.json(movies);
     }); 
 };
 
@@ -15,17 +25,46 @@ const show = (req, res) => {
     const movieId = parseInt(req.params.id);
 
     const querySql = `
-    SELECT *
-      FROM movies
+    SELECT 
+      movies.title,
+      movies.director,
+      movies.genre,
+      movies.release_year,
+      movies.abstract,
+      movies.image
+        FROM movies
     WHERE movies.id = ?;
+    `;
+
+    const queryReviewsSql = `
+    SELECT 
+      reviews.id,
+      reviews.name,
+      reviews.vote,
+      reviews.text
+        FROM reviews
+    WHERE movie_id = ?;
     `;
 
         connection.query(querySql, [movieId], (err, movieResults) => {
         if (err) return res.status(500).json({err: "Database query failed"});
         if (!movieResults.length) res.json("Movie not found");
 
-        const movie = movieResults[0];
-        res.json(movie);
+        connection.query(queryReviewsSql, [movieId], (err, reviewsResults) => {
+            if (err) return res.status(500).json({err: "Database query failed"});
+            if (!reviewsResults.length) res.json({err: "Reviews don't present"});
+            const reviews = reviewsResults;
+
+            const movie = {
+            ...movieResults[0], 
+            image: `http://localhost:3000/img/movies/${movieResults[0].image}`,
+            reviews
+            };
+
+            res.json(movie);
+        });
+
+        
         });
 };
 
